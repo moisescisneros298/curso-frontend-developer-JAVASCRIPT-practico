1ff00df5297efc2e1c8d091039f30eeb7f305abd
moises <moisescisneros298@gmail.com>
2022-10-09 14:37:46 -0500

    agregando el menu movile

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const movileMenu = document.querySelector('.mobile-menu');
 
 
 menuEmail.addEventListener('click', toggledesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+
+
 
 function toggledesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    movileMenu.classList.toggle('inactive');
 }
